ab903ca8612f311a0d73fd9186754782c2ed40a0
Michal Schielmann <michalschielmann@gmail.com>
2023-12-04 06:57:38 +0100

    aoc2023 - day4 solution

--- src/main/java/pl/mschielmann/aoc/year2023/day4/PuzzleSolver.java
+++ src/main/java/pl/mschielmann/aoc/year2023/day4/PuzzleSolver.java
@@ -2,6 +2,13 @@ package pl.mschielmann.aoc.year2023.day4;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Slf4j
 class PuzzleSolver
 {
@@ -12,13 +19,64 @@ class PuzzleSolver
         this.puzzleInput = puzzleInput;
     }
 
-    int solvePartOne()
+    long solvePartOne()
     {
-        return -1;
+        return puzzleInput.lines()
+                .map(ScratchCard::createScratchCardOf)
+                .map(ScratchCard::numberOfWinningCandidateCards)
+                .map(number -> Math.pow(2, number - 1))
+                .mapToLong(Double::longValue)
+                .sum();
     }
 
-    int solvePartTwo()
+    long solvePartTwo()
     {
-        return -1;
+        Map<Long, ScratchCard> scratchCardsById = puzzleInput.lines()
+                .map(ScratchCard::createScratchCardOf)
+                .collect(Collectors.toMap(ScratchCard::id, Function.identity()));
+
+        List<ScratchCard> scratchCardsToProcess = new LinkedList<>(scratchCardsById.values());
+        long numberOfCardsCollected = scratchCardsToProcess.size();
+        while (!scratchCardsToProcess.isEmpty())
+        {
+            ScratchCard currentCard = scratchCardsToProcess.remove(0);
+            int numberOfCardsAdded = scratchCardsToProcess.size();
+            long newToProcess = currentCard.numberOfWinningCandidateCards();
+            for (int counter = 1; counter <= newToProcess; counter++)
+            {
+                scratchCardsToProcess.add(scratchCardsById.get(currentCard.id + counter));
+            }
+            numberOfCardsAdded = scratchCardsToProcess.size() - numberOfCardsAdded;
+            numberOfCardsCollected += numberOfCardsAdded;
+        }
+
+        return numberOfCardsCollected;
+    }
+
+
+    record ScratchCard(Long id, List<Integer> winningNumbers, List<Integer> candidateNumbers)
+    {
+        private static ScratchCard createScratchCardOf(String line)
+        {
+            String[] idAndNumbers = line.split(":");
+            long id = Long.parseLong(idAndNumbers[0].replaceAll("\\s+", " ").split(" ")[1].trim());
+            String[] kindsOfNumbers = idAndNumbers[1].split("\\|");
+            List<Integer> winningNumbers = Stream.of(kindsOfNumbers[0].trim().split(" "))
+                    .filter(number -> !number.trim().isEmpty())
+                    .map(Integer::parseInt)
+                    .toList();
+            List<Integer> candidateNumbers = Stream.of(kindsOfNumbers[1].trim().split(" "))
+                    .filter(number -> !number.trim().isEmpty())
+                    .map(Integer::parseInt)
+                    .toList();
+            return new ScratchCard(id, winningNumbers, candidateNumbers);
+        }
+
+        long numberOfWinningCandidateCards()
+        {
+            return candidateNumbers().stream()
+                    .filter(winningNumbers::contains)
+                    .count();
+        }
     }
 }
