beb676a6493ba690f43d7ea30d8c2330329dc000
Michal Schielmann <michalschielmann@gmail.com>
2023-12-06 06:48:27 +0100

    aoc2023 - day6 solution

--- src/main/java/pl/mschielmann/aoc/year2023/day6/PuzzleSolver.java
+++ src/main/java/pl/mschielmann/aoc/year2023/day6/PuzzleSolver.java
@@ -2,23 +2,44 @@ package pl.mschielmann.aoc.year2023.day6;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.LongStream;
+
 @Slf4j
 class PuzzleSolver
 {
-    private final String puzzleInput;
+    private final List<RaceInfo> puzzleInput;
 
-    PuzzleSolver(String puzzleInput)
+    PuzzleSolver(List<RaceInfo> puzzleInput)
     {
         this.puzzleInput = puzzleInput;
     }
 
     long solvePartOne()
     {
-        return -1;
+        return puzzleInput.stream()
+                .map(this::getPossibleWinningWays)
+                .reduce(1L, (actual, partial) -> actual * partial);
+    }
+
+    private Long getPossibleWinningWays(RaceInfo race)
+    {
+        BigDecimal raceTime = BigDecimal.valueOf(race.time());
+        BigDecimal raceDistance = BigDecimal.valueOf(race.recordDistance());
+
+        return LongStream.range(1, race.time())
+                .mapToObj(BigDecimal::valueOf)
+                .filter(second -> second.multiply(raceTime.subtract(second)).compareTo(raceDistance) > 0)
+                .count();
     }
 
     long solvePartTwo()
     {
-        return -1;
+        return getPossibleWinningWays(puzzleInput.get(0));
+    }
+
+    record RaceInfo(long time, long recordDistance)
+    {
     }
 }
--- src/test/java/pl/mschielmann/aoc/year2023/day6/TestPuzzleSolverShould.java
+++ src/test/java/pl/mschielmann/aoc/year2023/day6/TestPuzzleSolverShould.java
@@ -2,10 +2,12 @@ package pl.mschielmann.aoc.year2023.day6;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import pl.mschielmann.aoc.year2023.day6.PuzzleSolver.RaceInfo;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Disabled
 class TestPuzzleSolverShould
 {
 
@@ -19,7 +21,7 @@ class TestPuzzleSolverShould
         long result = puzzleSolver.solvePartOne();
 
         // THEN
-        assertThat(result).isEqualTo(0L);
+        assertThat(result).isEqualTo(288L);
     }
 
     @Test
@@ -32,44 +34,53 @@ class TestPuzzleSolverShould
         long result = puzzleSolver.solvePartTwo();
 
         // THEN
-        assertThat(result).isEqualTo(0L);
+        assertThat(result).isEqualTo(71503L);
     }
 
     @Test
     void solvePartOneWithActualData()
     {
         // GIVEN
-        PuzzleSolver puzzleSolver = new PuzzleSolver(INPUT);
+        PuzzleSolver puzzleSolver = new PuzzleSolver(PART_1_INPUT);
 
         // WHEN
         long result = puzzleSolver.solvePartOne();
 
         // THEN
-        assertThat(result).isEqualTo(0L);
+        assertThat(result).isEqualTo(2065338L);
     }
 
     @Test
     void solvePartTwoWithActualData()
     {
         // GIVEN
-        PuzzleSolver puzzleSolver = new PuzzleSolver(INPUT);
+        PuzzleSolver puzzleSolver = new PuzzleSolver(PART_2_INPUT);
 
         // WHEN
         long result = puzzleSolver.solvePartTwo();
 
         // THEN
-        assertThat(result).isEqualTo(0L);
+        assertThat(result).isEqualTo(34934171L);
     }
 
-    private static final String TEST_INPUT_1 = """
-                        
-            """;
+    private static final List<RaceInfo> TEST_INPUT_1 = List.of(
+            new RaceInfo(7, 9),
+            new RaceInfo(15, 40),
+            new RaceInfo(30, 200)
+    );
 
-    private static final String TEST_INPUT_2 = """
-                        
-            """;
+    private static final List<RaceInfo> TEST_INPUT_2 = List.of(
+            new RaceInfo(71530, 940200)
+    );
 
-    static final String INPUT = """
-                        
-            """;
+    static final List<RaceInfo> PART_1_INPUT = List.of(
+            new RaceInfo(54, 446),
+            new RaceInfo(81, 1292),
+            new RaceInfo(70, 1035),
+            new RaceInfo(88, 1007)
+    );
+
+    static final List<RaceInfo> PART_2_INPUT = List.of(
+            new RaceInfo(54817088L, 446129210351007L)
+    );
 }
