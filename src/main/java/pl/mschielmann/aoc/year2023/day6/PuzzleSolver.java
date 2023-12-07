7f018260e0c4d2e5ed81fac2b84dd7af37ebd5d2
Michal Schielmann <michalschielmann@gmail.com>
2023-12-07 16:24:13 +0100

    aoc2023 - day6 alternative

--- src/main/java/pl/mschielmann/aoc/year2023/day6/PuzzleSolver.java
+++ src/main/java/pl/mschielmann/aoc/year2023/day6/PuzzleSolver.java
@@ -3,6 +3,8 @@ package pl.mschielmann.aoc.year2023.day6;
 import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.List;
 import java.util.stream.LongStream;
 
@@ -23,6 +25,11 @@ class PuzzleSolver
                 .reduce(1L, (actual, partial) -> actual * partial);
     }
 
+    long solvePartTwo()
+    {
+        return getPossibleWinningWaysUsingQuadraticFunction(puzzleInput.get(0));
+    }
+
     private Long getPossibleWinningWays(RaceInfo race)
     {
         BigDecimal raceTime = BigDecimal.valueOf(race.time());
@@ -34,9 +41,17 @@ class PuzzleSolver
                 .count();
     }
 
-    long solvePartTwo()
+    private long getPossibleWinningWaysUsingQuadraticFunction(RaceInfo race)
     {
-        return getPossibleWinningWays(puzzleInput.get(0));
+        BigDecimal raceTime = BigDecimal.valueOf(race.time());
+        BigDecimal raceDistance = BigDecimal.valueOf(race.recordDistance());
+
+        var delta = raceTime.multiply(raceTime).subtract(raceDistance.multiply(BigDecimal.valueOf(4)));
+        var solution1 = raceTime.negate().add(delta.sqrt(MathContext.DECIMAL128)).divide(BigDecimal.valueOf(-2), RoundingMode.DOWN).setScale(0, RoundingMode.DOWN).longValue();
+        var solution2 = raceTime.negate().subtract(delta.sqrt(MathContext.DECIMAL128)).divide(BigDecimal.valueOf(-2), RoundingMode.UP).setScale(0, RoundingMode.UP).longValue();
+        return LongStream.range(1, race.time())
+                .filter(second -> second > solution1 && second < solution2)
+                .count();
     }
 
     record RaceInfo(long time, long recordDistance)
